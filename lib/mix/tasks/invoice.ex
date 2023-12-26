3e2ce84f7becc4477784b327318ed53c4369f516
antony-slokar <antony.slokar@gmail.com>
2023-12-26 13:22:25 -0500

    Add the invoice mix task

---
+++ lib/mix/tasks/invoice.ex
@@ -0,0 +1,13 @@
+defmodule Mix.Tasks.Invoice do
+  @moduledoc """
+  Generate an invoice: `mix invoice`
+  """
+  use Mix.Task
+
+  def run(_) do
+    IO.puts "Generating invoice..."
+  end
+
+
+
+end
